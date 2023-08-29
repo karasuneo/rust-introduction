eb914b4d8f1bc8a87c8ff030c93b73fa03e4b05b
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-29 10:25:12 +0900

    i

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    // 変数の宣言、束縛(代入)
+    let message1 = "Hello, world!";
+    // !がついているものはマクロ、ついていないものは関数
+    // こうするとコンパイラが関数を探すときに楽になる
+    println!("{}", message1);
+
+    // letは再代入できない
+    // let message2 = "Hello, world! 2";
+    // let mut message3 = "Hello, world! 3";
+    // message3 = "Hello, world! 3-2";
 }
